688546ff4c18d08b5469021cedef69158ff67477
HelloWorld0328 <rdy00328@gmail.com>
2024-01-18 11:08:50 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState , useEffect} from "react";
+
+const App = () => {
+  const [comment,SetComment] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000');
+            const data = await response.json();
+            SetComment(data);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    fetchData();
+    const intervalId = setInterval(fetchData, 100);
+
+    return () => {
+        clearInterval(intervalId);
+    };
+}, []);
+
+const uploadComment = async () => {
+    const nameI = document.getElementById('nameInput').value;
+    const contentI = document.getElementById('contentInput').value;
+
+    fetch('http://localhost:8000', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: nameI, content: contentI }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('서버로부터의 응답:', data);
+    })
+    .catch(error => {
+        console.error('오류 발생:', error);
+    });
+}
+  return(
+    <div id="app" style={{
+      textAlign: "center",
+      paddingTop: "15px"
+    }}>
+      <div id="commentView">
+        {comment.map((item, idx) => (
+          <h3 key={idx}>{item.name} : {item.content}</h3>
+        ))}
+      </div> <br /><br /><br />
+      <div id="writeComment">
+        <input type="text" placeholder="이름" name="name" id="nameInput"/><br /><br />
+        <textarea id="contentInput" name="content" cols="22" rows="3" placeholder="내용"></textarea><br /><br />
+        <button id="uploadComment" onClick={()=>uploadComment()}>등록</button>
+      </div>
+    </div>
+  )
+}
+
+export default App;
